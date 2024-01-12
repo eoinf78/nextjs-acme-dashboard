040bd6175e133db4a7d6b48395f7fdb4828b1ec7
Eoin Fanning <eoin@hatchd.com.au>
2024-01-12 08:47:32 +0800

    Adding content to customer page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,32 @@
 import { Metadata } from 'next';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import { CustomerTableSkeleton } from '@/app/ui/skeletons';
+import Table from '@/app/ui/customers/table';
+import Search from '@/app/ui/search';
  
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({searchParams}:{
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-xl md:text-2xl`}>Customers</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customer..." />
+      </div>
+      <Suspense fallback={<CustomerTableSkeleton />}>
+        <Table query={query} />
+      </Suspense>
+    </div>
+  );
 }
